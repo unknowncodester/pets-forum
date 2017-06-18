271ca790f6354b3d64388cf29cc85c846e92c9b3
Dan <sgdhardy1991@gmail.com>
2017-06-18 22:41:15 +0100

    Added database migrations

---
+++ database/migrations/2017_06_18_211433_create_fixtures_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFixturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fixtures', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('home_team_id')->unsigned();
+            $table->integer('away_team_id')->unsigned();
+            $table->timestamps();
+            $table->foreign('home_team_id')->references('id')->on('teams');
+            $table->foreign('away_team_id')->references('id')->on('teams');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
