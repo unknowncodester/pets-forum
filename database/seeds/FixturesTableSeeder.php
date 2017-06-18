7bdf322c13d67579ff11ad8c06ce7f2df038cb86
Dan <sgdhardy1991@gmail.com>
2017-06-18 23:28:18 +0100

    Added function to generate all 760 fixtures for db seed

--- database/seeds/FixturesTableSeeder.php
+++ database/seeds/FixturesTableSeeder.php
@@ -13,16 +13,37 @@ class FixturesTableSeeder extends Seeder
     public function run()
     {
         Fixture::insert(
-            [
-                [
-                    "home_team_id" => 1,
-                    "away_team_id" => 2
-                ],
-                [
-                    "home_team_id" => 1,
-                    "away_team_id" => 3
-                ]
-            ]
+            $this->generateFixtures()
         );
+    }
+
+    private function generateFixtures()
+    {
+        $fixtures = [];
+
+        // create 38 fixtures for each team
+        for($teamOneId = 1; $teamOneId <= 20; $teamOneId++){
+
+            for($teamTwoId = 1; $teamTwoId <= 20; $teamTwoId++){
+
+                // cant play a game against yourself!
+                if($teamOneId != $teamTwoId)
+                {
+                    // home game
+                    $fixtures[] = [
+                        "home_team_id" => $teamOneId,
+                        "away_team_id" => $teamTwoId
+                    ];
+
+                    // away game
+                    $fixtures[] = [
+                        "home_team_id" => $teamTwoId,
+                        "away_team_id" => $teamOneId
+                    ];
+                }
+            }
+        }
+
+        return $fixtures;
     }
 }
