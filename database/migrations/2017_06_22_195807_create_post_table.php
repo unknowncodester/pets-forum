e12347d1a525ec27d15681f5222c5ac380b31c42
Dan <sgdhardy1991@gmail.com>
2017-06-22 21:17:57 +0100

    Foreign key remove temporarily

--- database/migrations/2017_06_22_195807_create_post_table.php
+++ database/migrations/2017_06_22_195807_create_post_table.php
@@ -20,8 +20,6 @@ class CreatePostTable extends Migration
             $table->integer('topic_id');
             $table->integer('user_id');
             $table->timestamps();
-            $table->foreign('user_id')->references('id')->on('users');
-            $table->foreign('topic_id')->references('id')->on('topics');
         });
     }
 
