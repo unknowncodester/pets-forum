758e741779a9748debf7c34de90eec7e8f2b9c43
Dan <sgdhardy1991@gmail.com>
2017-07-05 22:39:59 +0100

    Switch from raw query to query builder

--- app/Library/LeagueTable.php
+++ app/Library/LeagueTable.php
@@ -8,32 +8,34 @@ class LeagueTable
 {
     public function getLeagueStandings()
     {
-        $leagueStandings = DB::select(
-            'SELECT teams.name,
-                SUM(if(teams.id = matches.home_team_id
-                    OR teams.id = matches.away_team_id,1,0)) as games,
-                SUM(if(teams.id = matches.away_team_id
-                    AND matches.away_team_goals > matches.home_team_goals,1,0)) 
-                + SUM(if(teams.id = matches.home_team_id
-                    AND matches.home_team_goals > matches.away_team_goals,1,0)) AS wins,
-                SUM(IF(matches.away_team_goals = matches.home_team_goals,1,0)) AS draws,
-                SUM(if(teams.id = matches.away_team_id
-                    AND matches.home_team_goals > matches.away_team_goals,1,0)) 
-                + SUM(if(teams.id = matches.home_team_id
-                    AND matches.away_team_goals > matches.home_team_goals,1,0)) AS losses,
-                SUM(if(teams.id = matches.away_team_id
-                    AND matches.away_team_goals > matches.home_team_goals,3,0)) 
-                + SUM(if(teams.id = matches.home_team_id
-                    AND matches.home_team_goals > matches.away_team_goals,3,0))
-                +
-                SUM(IF(matches.away_team_goals = matches.home_team_goals,1,0)) AS points
-            FROM teams
-            INNER JOIN matches ON teams.id = matches.home_team_id
-                OR teams.id = matches.away_team_id
-            GROUP BY teams.name
-            ORDER BY points DESC'
-        );
-
-        return $leagueStandings;
+        return DB::table('teams')
+            ->join('matches', function ($join) {
+                $join->on('teams.id', '=', 'matches.home_team_id')->orOn('teams.id', '=', 'matches.away_team_id');
+            })
+            ->select(
+                DB::raw(
+                    'teams.name,
+                    SUM(if(teams.id = matches.home_team_id
+                        OR teams.id = matches.away_team_id,1,0)) as games,
+                    SUM(if(teams.id = matches.away_team_id
+                        AND matches.away_team_goals > matches.home_team_goals,1,0)) 
+                    + SUM(if(teams.id = matches.home_team_id
+                        AND matches.home_team_goals > matches.away_team_goals,1,0)) AS wins,
+                    SUM(IF(matches.away_team_goals = matches.home_team_goals,1,0)) AS draws,
+                    SUM(if(teams.id = matches.away_team_id
+                        AND matches.home_team_goals > matches.away_team_goals,1,0)) 
+                    + SUM(if(teams.id = matches.home_team_id
+                        AND matches.away_team_goals > matches.home_team_goals,1,0)) AS losses,
+                    SUM(if(teams.id = matches.away_team_id
+                        AND matches.away_team_goals > matches.home_team_goals,3,0)) 
+                    + SUM(if(teams.id = matches.home_team_id
+                        AND matches.home_team_goals > matches.away_team_goals,3,0))
+                    +
+                    SUM(IF(matches.away_team_goals = matches.home_team_goals,1,0)) AS points'
+                )
+            )
+            ->groupBy('teams.name')
+            ->orderBy('points', 'desc')
+            ->get();
     }
 }
